dc972a41137e3be3f3e9726a456219f55c151e96
Fabian Acero Garcia <acero01@gmail.com>
2019-02-05 17:26:15 -0500

    Paso el proyecto a springboot. Teniendo las funciones de crear cola, listar mensajes y enviar mensajes a la cola

--- src/main/java/com/acero/SimpleQueue.java
+++ src/main/java/com/acero/SimpleQueue.java
@@ -1,60 +1,20 @@
 package com.acero;
 
-import com.amazonaws.regions.Regions;
-import com.amazonaws.services.sqs.AmazonSQS;
-import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
-import com.amazonaws.services.sqs.model.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Properties;
-
+@SpringBootApplication
 public class SimpleQueue {
 
-    private static final String CONFIG_FILE = "src/main/resources/application.properties";
-    private static String QUEUE_NAME;
-
     public static void main(String[] args) {
-
-
-        try {
-            File file = new File(CONFIG_FILE);
-            FileReader reader = new FileReader(file);
-            Properties properties = new Properties();
-            properties.load(reader);
-            QUEUE_NAME = properties.getProperty("queue.name");
-            reader.close();
-        } catch (IOException e) {
-            System.exit(0);
-        }
-
-        AmazonSQS sqs;
-        sqs = AmazonSQSClientBuilder.standard()
-                .withRegion(Regions.US_EAST_1)
-                .build();
-        CreateQueueRequest create_request = new CreateQueueRequest(QUEUE_NAME)
-                .addAttributesEntry("DelaySeconds", "60")
-                .addAttributesEntry("MessageRetentionPeriod", "86400");
-
-        try {
-            GetQueueUrlResult queueUrl = sqs.getQueueUrl(QUEUE_NAME);
-            String queueUrlStr = queueUrl.getQueueUrl();
-            if(!queueUrlStr.equals("")){
-                throw new QueueNameExistsException("The queue already exist");
-            }
-        } catch (final QueueDoesNotExistException qdne) {
-            // Creating Queue
-            System.out.println("Creating Queue:\n");
-            System.out.println(sqs.createQueue(create_request));
-        } catch (final QueueNameExistsException qne) {
-            // Printing Queues
-            System.out.println("Your Queue already exists:\n");
-            ListQueuesResult queueList = sqs.listQueues();
-            for (String url : queueList.getQueueUrls()) {
-                System.out.println(url);
-                System.out.println("\n");
-            }
-        }
+        System.out.println("Hi");
+        SpringApplication.run(SimpleQueue.class, args);
+        /*try {
+            String queueUrl = getAWSQueue();
+            sendQueueMsj(queueUrl, "Test Messager" + (new Random()).nextInt(50));
+            receiveQueueMessages(queueUrl);
+        } catch (AmazonServiceException e) {
+        }*/
     }
+
 }
---
+++ src/main/java/com/acero/controller/AWSQueue.java
@@ -0,0 +1,126 @@
+package com.acero.controller;
+
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+import com.amazonaws.services.sqs.model.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import redis.clients.jedis.Jedis;
+
+import java.util.List;
+
+@RestController
+public class AWSQueue {
+
+    private static AmazonSQS sqs;
+    private static Jedis redisDb;
+    private static String queueUrl;
+
+    @Value("${redis.host}")
+    private String redisHost;
+
+    @Value("${redis.pwd}")
+    private String redisPwd;
+
+    public AWSQueue() {
+        sqs = AmazonSQSClientBuilder.standard()
+                .withRegion(Regions.US_EAST_1)
+                .build();
+    }
+
+    /**
+     * Crea una cola desde AWS
+     */
+    @RequestMapping("/create/{queueName}")
+    private String createAWSQueue(@PathVariable String queueName) {
+
+        String queueUrl = "";
+        StringBuilder responseQueueCreation = new StringBuilder("");
+        CreateQueueRequest create_request = new CreateQueueRequest(queueName)
+                .addAttributesEntry("DelaySeconds", "60")
+                .addAttributesEntry("MessageRetentionPeriod", "86400");
+        try {
+            GetQueueUrlResult remoteQueueUrl = sqs.getQueueUrl(queueName);
+            queueUrl = remoteQueueUrl.getQueueUrl();
+            if (!queueUrl.equals("")) {
+                throw new QueueNameExistsException("The queue already exist");
+            }
+        } catch (final QueueDoesNotExistException qdne) {
+            // Creating Queue
+            responseQueueCreation.append("Creating Queue:\n");
+            queueUrl = sqs.createQueue(create_request).getQueueUrl();
+            responseQueueCreation.append(queueUrl);
+            System.out.println(responseQueueCreation);
+        } catch (final QueueNameExistsException qne) {
+            // Printing Queues
+            responseQueueCreation.append("Your Queue already exists:\n");
+            ListQueuesResult queueList = sqs.listQueues();
+            for (String url : queueList.getQueueUrls()) {
+                responseQueueCreation.append(url + "\n");
+            }
+            System.out.println(responseQueueCreation);
+        }
+
+        saveQueueUrl(queueUrl);
+
+        return responseQueueCreation.toString();
+    }
+
+    /**
+     * Envia un mensaje a la cola seleccionada aws
+     *
+     * @param message Mensaje enviado a la cola
+     * @return boolean
+     */
+    @RequestMapping("/send/{message}")
+    private String sendQueueMsj(@PathVariable String message) {
+
+        if (getRecentQueueUrl()) return "No queue defined";
+        return sqs.sendMessage(new SendMessageRequest(queueUrl, message)).toString();
+    }
+
+    /**
+     * Receive all queue Messages
+     *
+     * @param queueUrl
+     */
+    @RequestMapping("/list/{queueName}")
+    private String receiveQueueMessages(@PathVariable String queueUrl) {
+        if (getRecentQueueUrl()) return "No queue defined";
+        ReceiveMessageRequest receiveMessageRequest = new ReceiveMessageRequest(queueUrl);
+        List<Message> messages = sqs.receiveMessage(receiveMessageRequest).getMessages();
+        StringBuilder messageListResponde = new StringBuilder("");
+
+        for (Message message : messages) {
+            messageListResponde.append(message.getBody() + "<br />");
+        }
+        System.out.println(messageListResponde);
+        return messageListResponde.toString();
+    }
+
+    /**
+     * Guarda la url de la cola recientemente creada
+     *
+     * @param queueUrl
+     */
+    private void saveQueueUrl(String queueUrl) {
+        redisDb = new Jedis(redisHost);
+        redisDb.auth(redisPwd);
+        redisDb.set("queue-url", queueUrl);
+    }
+
+    /**
+     * Obtiene la url de la ultima cola creada
+     *
+     * @return
+     */
+    private boolean getRecentQueueUrl() {
+        redisDb = new Jedis(redisHost);
+        redisDb.auth(redisPwd);
+        queueUrl = redisDb.get("queue-url");
+        return queueUrl.isEmpty();
+    }
+}
